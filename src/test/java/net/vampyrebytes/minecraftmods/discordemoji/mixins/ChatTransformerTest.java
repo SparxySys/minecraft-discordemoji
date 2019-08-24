42ef0d0142414ff6f48beb961eb2d4e37bf7536a
Christina Lena Korosec <christina.korosec@ds003.info>
2019-08-24 22:38:16 +0200

    chat transformer test value assertion

--- src/test/java/net/vampyrebytes/minecraftmods/discordemoji/mixins/ChatTransformerTest.java
+++ src/test/java/net/vampyrebytes/minecraftmods/discordemoji/mixins/ChatTransformerTest.java
@@ -1,5 +1,6 @@
 package net.vampyrebytes.minecraftmods.discordemoji.mixins;
 
+import net.minecraft.util.text.TextComponentString;
 import net.vampyrebytes.minecraftmods.discordemoji.EmojiTransformer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +9,9 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.lang.reflect.Field;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -28,10 +31,14 @@ public class ChatTransformerTest {
         instanceField.setAccessible(true);
         instanceField.set(null, emojiTransformer);
 
+        final String input = "Hello this is some text";
+
         when(chatTransformer.getFormattedText()).thenCallRealMethod();
+        when(chatTransformer.stream()).thenReturn(Stream.of(new TextComponentString(input)));
 
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         chatTransformer.getFormattedText();
         verify(emojiTransformer).transformEmojis(captor.capture());
+        assertThat(captor.getValue()).isEqualTo(input);
     }
 }
