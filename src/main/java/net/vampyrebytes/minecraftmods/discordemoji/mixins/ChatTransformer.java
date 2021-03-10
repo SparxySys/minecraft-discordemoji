a43b7579face5546cb9a6997750c33bc9a518383
Christina Lena Korosec <christina.korosec@vampyrebytes.net>
2021-03-10 20:26:54 +0100

    wip 1.16.5 compat

--- src/main/java/net/vampyrebytes/minecraftmods/discordemoji/mixins/ChatTransformer.java
+++ src/main/java/net/vampyrebytes/minecraftmods/discordemoji/mixins/ChatTransformer.java
@@ -1,6 +1,10 @@
 package net.vampyrebytes.minecraftmods.discordemoji.mixins;
 
+import java.util.Optional;
+
 import net.minecraft.text.BaseText;
+import net.minecraft.text.OrderedText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.vampyrebytes.minecraftmods.discordemoji.EmojiTransformer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,8 +13,16 @@ import org.spongepowered.asm.mixin.Mixin;
 public abstract class ChatTransformer implements Text {
 
     @Override
-    public String getString() {
-        String text = Text.super.getString();
-        return EmojiTransformer.getInstance().transformEmojis(text);
+    public <T> Optional<T> visitSelf(StyledVisitor<T> visitor, Style style) {
+        return visitor.accept(style, asTransformedString());
+    }
+
+    @Override
+    public <T> Optional<T> visitSelf(Visitor<T> visitor) {
+        return visitor.accept(asTransformedString());
+    }
+
+    String asTransformedString() {
+        return EmojiTransformer.getInstance().transformEmojis(asString());
     }
 }
--- src/test/java/net/vampyrebytes/minecraftmods/discordemoji/mixins/ChatTransformerTest.java
+++ src/test/java/net/vampyrebytes/minecraftmods/discordemoji/mixins/ChatTransformerTest.java
@@ -1,6 +1,9 @@
 package net.vampyrebytes.minecraftmods.discordemoji.mixins;
 
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.OrderedText;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
 import net.vampyrebytes.minecraftmods.discordemoji.EmojiTransformer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +12,11 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.lang.reflect.Field;
-import java.util.stream.Stream;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -21,9 +26,6 @@ public class ChatTransformerTest {
     @Mock
     private EmojiTransformer emojiTransformer;
 
-    @Mock
-    private ChatTransformer chatTransformer;
-
     @Test
     public void delegatesToEmojiTransformerCorrectly() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
         Class<?> instanceHolder = Class.forName(EmojiTransformer.class.getCanonicalName() + "$InstanceHolder");
@@ -32,13 +34,43 @@ public class ChatTransformerTest {
         instanceField.set(null, emojiTransformer);
 
         final String input = "Hello this is some text";
-
-        when(chatTransformer.getString()).thenCallRealMethod();
-        when(chatTransformer.of()).thenReturn(new LiteralText(input));
+        DummyChatTransformer chatTransformer = new DummyChatTransformer(input);
 
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         chatTransformer.getString();
         verify(emojiTransformer).transformEmojis(captor.capture());
         assertThat(captor.getValue()).isEqualTo(input);
+    }
+
+    private static class DummyChatTransformer extends ChatTransformer {
+        private final String input;
+
+        private DummyChatTransformer(String input) {
+            this.input = input;
+        }
+
+        @Override public Style getStyle() {
+            return null;
+        }
+
+        @Override public String asString() {
+            return input;
+        }
+
+        @Override public List<Text> getSiblings() {
+            return Collections.emptyList();
+        }
+
+        @Override public MutableText copy() {
+            return null;
+        }
+
+        @Override public MutableText shallowCopy() {
+            return null;
+        }
+
+        @Override public OrderedText asOrderedText() {
+            return null;
+        }
     }
 }
